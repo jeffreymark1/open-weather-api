a2ce08c2ae426b22f2e1eb238c012d1edd341bb9
Jeff G <jeffg@Jeffs-MBP.hsd1.pa.comcast.net>
2023-09-09 04:21:22 -0400

    added :focus on button with blue background

--- script.js
+++ script.js
@@ -10,10 +10,23 @@ button.addEventListener('click', async () => {
     let inputname = input.value;
     let response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${inputname}&appid=b2f3762212a93432cb01ba054a297a38&units=imperial`)
     document.querySelector("#cityName").innerText=`${inputname}`
-    document.querySelector("#temp").innerText = `${inputname} temperature is: ${response.data.main.temp}°F`
-    console.log(response.data.main.temp)
+    document.querySelector("#temp").innerText = `${inputname} temperature is: ${Math.round(response.data.main.temp)}°F`
+    console.log(Math.round(response.data.main.temp));
     console.log(inputname)
-})
+    console.log(response)
+});
+
+
+button.addEventListener('keydown', async (event) => {
+    if (event.keyCode === 13) {
+    let inputname = input.value;
+    let response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${inputname}&appid=b2f3762212a93432cb01ba054a297a38&units=imperial`)
+    document.querySelector("#cityName").innerText=`${inputname}`
+    document.querySelector("#temp").innerText = `${inputname} temperature is: ${Math.round(response.data.main.temp)}°F`
+    console.log(Math.round(response.data.main.temp));
+    console.log(inputname)
+    }
+});
 
 function show() {
    document.querySelector('#image')
@@ -21,9 +34,3 @@ function show() {
     document.querySelector('#submitButton')
         .style.display = "none";
 }
-
-// button.addEventListener('click', (e) => {
-//     //axios.get('https://api.openweathermap.org/data/2.5/weather?q=philadelphia&appid=b2f3762212a93432cb01ba054a297a38');
-//     alert("button");
-//     console.log(input.value);
-//})
